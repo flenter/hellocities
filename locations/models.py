e19747ba5698fb17794f97d60bf63fcf51be86aa
Jacco Flenter <github@putpixel.nl>
2013-11-14 14:21:54 +0100

    forgot to update locations/models.py

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,19 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=150)
+    geom = models.PointField()
+    objects = models.GeoManager()
+
+    class Meta:
+        ordering = ['name']
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def get_lat_long(self):
+        """Add an easy getter function, which returns the geometry coords in
+        latitude longitude order
+        """
+        return (self.geom.coords[1], self.geom.coords[0])
