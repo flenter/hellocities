71b01c5b52fd5289f909601563ed2c1ac6005c65
Jacco Flenter <github@putpixel.nl>
2013-11-27 22:27:45 +0100

    first test with selenium

--- locations/tests.py
+++ locations/tests.py
@@ -52,3 +52,32 @@ class DistanceFilterCase(TestCase):
 
         result = distance.calc_distance(b, a)
         self.assertEqual(result, 8794.624631776502)
+
+import re
+
+from django.test import LiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class MySeleniumTests(LiveServerTestCase):
+    fixtures = ["data/locations.json"]
+
+    @classmethod
+    def setUpClass(cls):
+        cls.selenium = WebDriver()
+        super(MySeleniumTests, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_locations(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/locations/'))
+        print(dir(self.selenium))
+        src = self.selenium.page_source
+        text_found = re.search(r'text_to_search', src)
+        self.assertFalse(text_found)
+        text_found = re.search(r'Amsterdam', src)
+        self.assertTrue(text_found)
+    # self.selenium.find_element_by_xpath('//input[@value="Log in"]').click()
